be7fdb6e3d113dfb3b6438af1ecc26f2cd13a998
woals990605 <woals990605@naver.com>
2022-03-08 16:24:24 +0900

    10.인증필터링을 위한  주소설계로 변경하기

--- dbproject/src/main/java/site/metacoding/dbproject/web/PostController.java
+++ dbproject/src/main/java/site/metacoding/dbproject/web/PostController.java
@@ -11,46 +11,46 @@ import org.springframework.web.bind.annotation.PutMapping;
 @Controller
 public class PostController {
 
-    // GET 글쓰기 페이지 /post/writeForm
-    @GetMapping("/post/writeForm")
+    // GET 글쓰기 페이지 /post/writeForm-인증O
+    @GetMapping("/s/post/writeForm")
     public String writeForm() {
         return "post/writeForm";
     }
 
-    // 메인페이지
+    // 메인페이지-인증X
     // GET 글목록 페이지 /post/list, /
     // @GetMapping({"/", "/post/list"})
-    @GetMapping({ "/", "/post/list" })
+    @GetMapping({ "/", "post/list" })
     public String list() {
         return "post/list";
     }
 
-    // GET 글상세보기 페이지 /post/{id} (삭제버튼 만들어 두면됨, 수정버튼 만들어 두면됨)
-    @GetMapping("/post/{id}")
+    // GET 글상세보기 페이지 /post/{id} (삭제버튼 만들어 두면됨, 수정버튼 만들어 두면됨)-인증X
+    @GetMapping("/post/{id}") // Get요청에 /post 제외 시키기
     public String detail(@PathVariable Integer id) {
         return "post/detail";
     }
 
-    // GET 글수정 페이지 /post/{id}/updateForm
-    @GetMapping("/post/{id}/updateForm")
+    // GET 글수정 페이지 /post/{id}/updateForm-인증O
+    @GetMapping("/s/post/{id}/updateForm")
     public String updateForm(@PathVariable Integer id) {
         return "post/updateForm"; // ViewResolver 도움 받음.
     }
 
-    // DELETE 글삭제 /post/{id} - 글목록으로 가기
-    @DeleteMapping("/post/{id}")
+    // DELETE 글삭제 /post/{id} - 글목록으로 가기-인증O
+    @DeleteMapping("/s/post/{id}")
     public String delete(@PathVariable Integer id) {
         return "redirect:/";
     }
 
-    // UPDATE 글수정 /post/{id} - 글상세보기 페이지가기
-    @PutMapping("/post/{id}")
+    // UPDATE 글수정 /post/{id} - 글상세보기 페이지가기-인증O
+    @PutMapping("/s/post/{id}")
     public String update(@PathVariable Integer id) {
         return "redirect:/post/" + id;
     }
 
-    // POST 글쓰기 /post - 글목록으로 가기
-    @PostMapping("/post")
+    // POST 글쓰기 /post - 글목록으로 가기-인증O
+    @PostMapping("/s/post")
     public String write() {
         return "redirect:/";
     }
