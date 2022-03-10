aa1ca3a56d7ee7d5946abc19cb74c701110ecf21
woals990605 <woals990605@naver.com>
2022-03-11 00:42:08 +0900

    빅데이터 서버프로그램구현완료

--- serverproject/src/main/java/site/metacoding/serverproject/web/DownloadController.java
+++ serverproject/src/main/java/site/metacoding/serverproject/web/DownloadController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,15 +17,13 @@ import site.metacoding.serverproject.domain.HospitalRepository;
 public class DownloadController {
 
     private HospitalRepository hospitalRepository;
-    private HttpSession session;
 
     public DownloadController(HospitalRepository hospitalRepository, HttpSession session) {
         this.hospitalRepository = hospitalRepository;
-        this.session = session;
     }
 
     @GetMapping("/download")
-    public String download(Hospital hospital) {
+    public String download(Hospital hospital, Model model) {
         // 1. URI로 다운로드
         RestTemplate rt = new RestTemplate();
         Hospital[] response = rt.getForObject("http://3.38.254.72:5000/api/hospital?sidoCdNm=부산&sgguCdNm=부산사하구",
@@ -33,8 +32,15 @@ public class DownloadController {
         List<Hospital> hosList = Arrays.asList(response);
 
         // 2. DB에 saveAll() + model에 담기
+        hospitalRepository.saveAll(hosList);
+        model.addAttribute("Hospitals", hospitalRepository.findAll());
 
         // 3. 리턴
         return "list";
+    }
+
+    @GetMapping("/main")
+    public String main() {
+        return "/main";
     }
 }
